84ba061f7033f07d1fe4e0c56bbf4be2a21dfc16
Nicolasknuppmendes <104834111+Nicolasknuppmendes@users.noreply.github.com>
2024-09-27 16:39:02 +0000

    .

--- graficos/quantiadedeUsuarios.js
+++ graficos/quantiadedeUsuarios.js
@@ -1,24 +1,61 @@
-async function quantidadeUsuarios() {
-    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
+import { getCSS, tickConfig } from "./common.js"
+
+async function quantidadeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
     const nomeDasRedes = Object.keys(dados)
-    const quantidadeUsuarios = Object.values(dados)
-  
-  const data = [
-    {
-      x: nomeDasRedes,
-      y: quantidadeUsuarios,
-      type: 'bar'
+    const quantidadeDeUsuarios = Object.values(dados)
+
+    const data = [
+        {
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
+            type: 'bar',
+            marker: {
+                color: getCSS('--primary-color')
+            }
+        }
+    ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+            text: 'Redes sociais com mais usuários no mundo',
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                family: getCSS('--font'),
+                size: 30
+            }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'nome das redes sociais',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'bilhões de usuários ativos',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        }
+
     }
-  ]
-  
-  const grafico = document.createElement('div')
-  grafico.className = 'grafico'
-  document.getElementById('graficos-container').appendChild(grafico)
-  Plotly.newPlot(grafico, data)
-  
-  }
-  
-  quantidadeUsuarios()
+
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, layout)
+}
+
+quantidadeUsuariosPorRede()
   
